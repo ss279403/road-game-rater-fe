90ce6e2d9ca9c8b9d1ca2da87518410cf6bd59cf
Sam Spiers <sam.spiers@libertymutual.com>
2018-01-09 18:41:19 -0500

    working map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataService } from './data.service';
@@ -10,12 +10,13 @@ import { AgmCoreModule } from '@agm/core';
 //import { MatDialogModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
 //import { AppRoutingModule } from './routing/routing.module';
-
+import { MapComponent } from './map/map.component';
 //import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent ],
+    AppComponent,
+    MapComponent ],
   imports: [
     BrowserModule,
   //  AppRoutingModule,
@@ -24,8 +25,11 @@ import { HttpModule } from '@angular/http';
     HttpModule,
   //  MatDialogModule,
     FormsModule,
+    ReactiveFormsModule,
       AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCIGoNBEP7o7It5pNj2RHtSz0B6_uf9P_U'
+      //  apiKey: "AIzaSyAdnWstnGpjYaJZIfWabH0woBiZnZJEHig",
+      apiKey: "AIzaSyCIGoNBEP7o7It5pNj2RHtSz0B6_uf9P_U",
+      libraries: ["places"]
       }),
     ],
   providers: [DataService],
