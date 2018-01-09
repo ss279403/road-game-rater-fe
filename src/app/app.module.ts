fc9c18deedea0bf9b3794c4300d99e9a819a52b1
Sam Spiers <sam.spiers@libertymutual.com>
2018-01-09 16:35:33 -0500

    adding form to submit review to java

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataService } from './data.service';
 import { AgmCoreModule } from '@agm/core';
+// import { RouterModule }   from '@angular/router';
+//import { MatDialogModule } from '@angular/material';
+import { HttpModule } from '@angular/http';
+//import { AppRoutingModule } from './routing/routing.module';
+
+//import { environment } from '../environments/environment';
 
 @NgModule({
+  declarations: [
+    AppComponent ],
   imports: [
     BrowserModule,
+  //  AppRoutingModule,
+    BrowserAnimationsModule,
     CommonModule,
+    HttpModule,
+  //  MatDialogModule,
     FormsModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAR1X84QtIjJRNdWO_wl4MaPYJ0hY24KGY'
-    })
-  ],
-  providers: [],
-  declarations: [ AppComponent ],
+      AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCIGoNBEP7o7It5pNj2RHtSz0B6_uf9P_U'
+      }),
+    ],
+  providers: [DataService],
+
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule {}
