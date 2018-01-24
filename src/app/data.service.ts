7a7a5a71e7277544930ce40a2d069ddc234d214c
Sam Spiers <sam.spiers@libertymutual.com>
2018-01-24 16:39:11 -0500

    setting up for heroku

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DataService {
 
-    private baseUrl = 'http://localhost:8080/api/'
+    private baseUrl = 'https://damp-anchorage-45923.herokuapp.com/api/'
     private headers = new Headers({ 'Content-Type': 'application/json' });
     private options = new RequestOptions({ headers: this.headers, withCredentials: true });
 
